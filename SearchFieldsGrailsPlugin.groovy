c584b9dfe21c74926856f6d54ac18e10d2bd60c4
leandrogehlen <leandrogehlen@gmail.com>
2013-06-18 10:31:07 -0300

    Changed to version 0.2

--- SearchFieldsGrailsPlugin.groovy
+++ SearchFieldsGrailsPlugin.groovy
@@ -3,7 +3,7 @@ import org.grails.plugins.search.util.SearchUtil
 
 class SearchFieldsGrailsPlugin {
 
-	def version = "0.1"
+	def version = "0.2"
 	def grailsVersion = "2.0 > *"
 
 	def pluginExcludes = [
